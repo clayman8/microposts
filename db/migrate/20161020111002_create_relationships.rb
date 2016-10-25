b05b5c36da003179f13a08e7ec6abeef76692bba
hiroki hotta <pwpcg756@yahoo.co.jp>
2016-10-25 14:20:56 +0000

    fix add followings/followers

--- db/migrate/20161020111002_create_relationships.rb
+++ db/migrate/20161020111002_create_relationships.rb
@@ -10,8 +10,5 @@ class CreateRelationships < ActiveRecord::Migration
       
       t.index [:follower_id, :followed_id], unique: true
     end
-    add_index :relationships, :follower_id
-    add_index :relationships, :followed_id
-    add_index :relationships, [:follower_id, :followed_id], unique: true
   end
 end
